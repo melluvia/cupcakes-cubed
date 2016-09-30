24ad11a68833b81c1f461a654020350f919f23fd
Melissa Phillips <melissanicolephillips@gmail.com>
2016-09-30 14:08:09 -0500

    Inital concept
    Initial concept for cupcakes cubed app including storyboard
    
    Signed-off-by: Melissa Phillips <melissanicolephillips@gmail.com>

---
+++ SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  cupcakes-cubed
+//
+//  Created by Melissa Phillips on 9/29/16.
+//  Copyright © 2016 Melissa Phillips Design. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
