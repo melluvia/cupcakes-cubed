f0d90706c717bc6d0568b93f9e263630024280ac
Melissa Phillips <melissanicolephillips@gmail.com>
2016-10-17 16:06:11 -0500

    new art, autolayout, segues
    Signed-off-by: Melissa Phillips <melissanicolephillips@gmail.com>

--- OrderTableViewController.swift
+++ OrderTableViewController.swift
@@ -13,7 +13,15 @@ class OrderTableViewController: UIViewController, UINavigationControllerDelegate
 	@IBOutlet weak var tax: UILabel!
 	@IBOutlet weak var deliveryCost: UILabel!
 	@IBOutlet weak var tableView: UITableView!
-
+	
+	var order = ShoppingCart.sharedInstance.shoppingCartArray
+	
+	func onEditBtn(sender: UIBarButtonItem) {
+		
+		self.tableView.isEditing = !self.tableView.isEditing
+	}
+	
+//	let order = ShoppingCart.sharedInstance.shoppingCartArray[(indexPath.row)]
 	// MARK: Properties
 	
 	// Create a cache that uses keys of type NSString to point to types of UIImage.
@@ -28,10 +36,20 @@ class OrderTableViewController: UIViewController, UINavigationControllerDelegate
 
 		imageCache.countLimit = 50 // Cache up to 50 UIImage(s)
 
-		// Use the edit button item provided by the table view controller.
-		navigationItem.leftBarButtonItem = editButtonItem
+		let img = UIImage(named: "my-bag")!.withRenderingMode(UIImageRenderingMode.alwaysOriginal)
 
-		//how to load the cached flavor, image, price and amout??
+		// Use the edit button item provided by the table view controller.
+		let rightBarButtonItem = UIBarButtonItem(image: img,
+		                                        style: UIBarButtonItemStyle.plain,
+		                                        target: self,
+		                                        action: #selector(onEditBtn(sender:)))
+		
+		
+		self.navigationItem.rightBarButtonItem = rightBarButtonItem
+		
+		self.tableView.reloadData()
+		
+		order += loadOrderFromArchiver()!
 
 	}
 
@@ -85,6 +103,64 @@ class OrderTableViewController: UIViewController, UINavigationControllerDelegate
 		
 		return cell
 	}
+	
+	// Override to support editing the table view.
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//		let order = ShoppingCart.sharedInstance.shoppingCartArray[(indexPath as NSIndexPath).row]
+		if editingStyle == .delete {
+			
+			// Find the OrderData in the data source that we wish to delete.
+			let orderToRemove = order[indexPath.row]
+			
+			//have to remove orders from a singleton?
+//			ShoppingCart.sharedInstance.removeOrder(orderToRemove: orderToRemove,
+			
+//			  completion: {
+//				
+//				self.order.remove(at: (indexPath as NSIndexPath).row)
+//				tableView.deleteRows(at: [indexPath], with: .fade)
+//			},
+//			  
+//			  error: {
+//				let alertController = UIAlertController(title: "Remove Failed",
+//														message: "Oops! We couldn't remove your Order at this time.",
+//														preferredStyle: .alert)
+//				
+//				let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+//						alertController.addAction(okAction)
+//					
+//				self.present(alertController, animated: true, completion: nil)
+//			})
+//			
+//		} else {
+			
+			// Delete the row from the data source
+			order.remove(at: (indexPath as NSIndexPath).row)
+				
+			// Save the meals.
+			saveOrderToArchiver()
+			
+			tableView.deleteRows(at: [indexPath], with: .fade)
+		}
+	}
+	
+	// MARK: NSCoding
+	
+	func saveOrderToArchiver() {
+		
+		let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(order, toFile: OrderData.ArchiveURL.path)
+		
+		if !isSuccessfulSave {
+			print("Failed to save orders...")
+		}
+	}
+	
+	func loadOrderFromArchiver() -> [OrderData]? {
+		
+		return NSKeyedUnarchiver.unarchiveObject(withFile: OrderData.ArchiveURL.path) as? [OrderData]
+	}
+
+
 
 //		if imageCache.object(forKey: order.itemPhoto as NSString) != nil {
 //				
@@ -94,7 +170,6 @@ class OrderTableViewController: UIViewController, UINavigationControllerDelegate
 //		}
 }
 
-//add singleton to this
 
 
 
--- ShoppingCartManager.swift
+++ ShoppingCartManager.swift
@@ -8,8 +8,29 @@
 
 import Foundation
 
-class ShoppingCartManager {
-	
-	private var shoppingCart = [ShoppingCart]()
-	
-	}
+//class ShoppingCartManager {
+//	
+//	private var shoppingCart = [ShoppingCart]()
+//	
+//	func removeOrder(orderToRemove: OrderData, completion: @escaping () -> (), error: @escaping () -> ()) {
+//        
+//        print("Remove Order: \(orderToRemove.flavor)")
+//        
+//        let dataStore = shoppingCart.description.
+//        
+//        _ = dataStore?.removeID(orderToRemove.objectId,
+//                                
+//            response: { (result: NSNumber?) -> Void in
+//
+//                print("One Order has been removed: \(result)")
+//                completion()
+//            },
+//            
+//            error: { (fault: Fault?) -> Void in
+//                print("Failed to remove Order: \(fault)")
+//                error()
+//            }
+//        )
+//    }
+
+
